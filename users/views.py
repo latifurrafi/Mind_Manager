94cf5d8baf6385225975e10fdc559a62903a0bb6
latifurrafi <latifurrafi@gmail.com>
2025-06-30 22:05:53 +0600

    Initial:Commit

---
+++ users/views.py
@@ -0,0 +1,16 @@
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
+from django.shortcuts import render
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+def index(request):
+    total_tasks = Task.objects.count()
+    return render(request, 'index.html',{
+        'total_tasks': total_tasks
+    })
